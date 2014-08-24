111febe203fc63b5ece25d235d0be43ec32059c9
Andrew Hyatt <ahyatt@gmail.com>
2014-08-23 20:56:20 -0400

    Fix `websocket-version': update to 1.3

--- websocket.el
+++ websocket.el
@@ -99,7 +99,7 @@ same for the protocols.
   accept-string
   (inflight-input nil))
 
-(defvar websocket-version "1.1"
+(defvar websocket-version "1.3"
   "Version numbers of this version of websocket.el.")
 
 (defvar websocket-debug nil
